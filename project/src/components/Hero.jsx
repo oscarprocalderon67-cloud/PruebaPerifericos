5cfbebe6bd3185f8fe5de9844a9e956e66ae1d55
Oscar Calderón <oscarprocalderon67@gmail.com>
2026-03-17 13:57:55 -0500

    FONDO LOGO

--- project/src/components/Hero.jsx
+++ project/src/components/Hero.jsx
@@ -1,7 +1,12 @@
+import logoImg from '../img/logoJOMTECH.jpeg';
+
 export default function Hero() {
   return (
     <section className="hero">
-      <div className="container">
+      <div className="hero-bg-logo">
+        <img src={logoImg} alt="JOMTECH Background" />
+      </div>
+      <div className="container hero-container">
         <div className="hero-content">
           <h1>Periféricos de Calidad</h1>
           <p>Encuentra todo lo que necesitas para tu setup tecnológico</p>
